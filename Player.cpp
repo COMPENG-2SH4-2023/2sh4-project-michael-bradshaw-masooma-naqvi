7a6fb9a85eedec7cab8ac175a2277586ece84a7a
Masooma Naqvi <naqvim18@mcmaster.ca>
2023-11-30 09:17:46 -0500

    Iteration 1

--- Player.cpp
+++ Player.cpp
@@ -7,26 +7,113 @@ Player::Player(GameMechs* thisGMRef)
     myDir = STOP;
 
     // more actions to be included
+    playerPos.setObjPos(mainGameMechsRef->getBoardSizeX()/2, mainGameMechsRef->getBoardSizeY()/2, '*');
+
+    // no heap member yet - never used new keyword
 }
 
 
 Player::~Player()
 {
     // delete any heap members here
+
+    // we can leave it empty for now
 }
 
 void Player::getPlayerPos(objPos &returnPos)
 {
+    returnPos.setObjPos(playerPos.x, playerPos.y, playerPos.symbol);
     // return the reference to the playerPos arrray list
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic 
+    
+    char input = mainGameMechsRef->getInput();
+
+    switch(input) 
+    {
+        case 'w':
+            if(myDir != UP && myDir != DOWN)
+            {
+                myDir = UP;
+            }
+            break;
+
+        case 'a':
+            if(myDir != LEFT && myDir != RIGHT)
+            {
+                myDir = LEFT;
+            }
+            break;
+
+        case 's':
+            if (myDir != UP && myDir != DOWN)
+            {
+                myDir = DOWN;
+            }
+            break;
+
+        case 'd':
+            if(myDir != LEFT && myDir != RIGHT)
+            {
+                myDir = RIGHT;
+            }
+            break;
+        
+        default:
+            break;
+    }      
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    switch(myDir) 
+    {
+        // player wants to move up
+        case UP:
+            playerPos.y--;
+            if(playerPos.y <0)
+            {
+                playerPos.y = mainGameMechsRef->getBoardSizeY()-1;
+            }
+            break;
+
+        // player wants to move down
+        case DOWN:
+            playerPos.y++;
+            if(playerPos.y >= mainGameMechsRef->getBoardSizeY())
+            {
+                playerPos.y = 0;
+            }
+            break;
+
+        // player wants to move left
+        case LEFT:
+            playerPos.x--;
+            if(playerPos.x < 0)
+            {
+                playerPos.x = mainGameMechsRef->getBoardSizeX()-1;
+            }
+            break;
+
+        // player wants to move right
+        case RIGHT:
+            playerPos.x++;
+            if(playerPos.x >= mainGameMechsRef->getBoardSizeX())
+            {
+                playerPos.x = 0;
+            }
+            break;
+
+        // default case
+        default:
+            break;
+
+        
+
+    }
 }
 
--- Project.cpp
+++ Project.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
-
+#include "GameMechs.h"
+#include "Player.h"
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
-bool exitFlag;
+GameMechs* myGameMech;
+Player* myPlayer;
+
+//char gameBoard[myGM->getBoardSizeX, myGM->getBoardSizeY];
+
+// Dr. Chen Video 1
+//objPos myPos;
+
+//bool exitFlag;
+
+
 
 void Initialize(void);
 void GetInput(void);
@@ -23,7 +34,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(!myGameMech->getExitFlagStatus() && !myGameMech->getLoseFlagStatus())  
     {
         GetInput();
         RunLogic();
@@ -41,22 +52,77 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    // Iteration 0: Adding a few arbitrarily chosen coordinates and symbols
+    //myPos.setObjPos(2, 3, '@');
+
+    myGameMech = new GameMechs();//new GameMechs(26, 13); // makes the board size 26 and 13
+    myPlayer = new Player(myGameMech);
+    
 }
 
 void GetInput(void)
 {
+   myGameMech->getInput(); 
    
 }
 
 void RunLogic(void)
 {
-    
+    if(myGameMech->getInput() == 27)
+    {
+        myGameMech->setExitTrue(); 
+    }
+
+    // update the FSM and move the player
+    myPlayer->updatePlayerDir(); 
+    myPlayer->movePlayer(); 
+
+    myGameMech->clearInput(); // so to not repeatedly process input;  
+
 }
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    
+    objPos tempPos;
+    // getting the player position
+    myPlayer->getPlayerPos(tempPos);
+
+    //for(int i = 0; i < 1000; i++)
+    //{
+    //    MacUILib_printf("BoardSize: %dx%d, PlayerPos <%d,%d> + %c\n", myGM->getBoardSizeX(), myGM->getBoardSizeY(),
+    //    tempPos.x, tempPos.y, tempPos.symbol);
+
+    //    MacUILib_clearScreen();
+
+    //}
+    MacUILib_clearScreen();
+     
+      for(int i = 0; i < myGameMech->getBoardSizeY(); i++){
+          for(int j = 0; j< myGameMech->getBoardSizeX(); j++)
+          {
+              if(i == 0 || i == (myGameMech->getBoardSizeY()-1)|| j == 0 || j == (myGameMech->getBoardSizeX()-1))
+              {
+                 MacUILib_printf("#");
+              }
+
+              else if(tempPos.y == i && tempPos.x == j)
+              {
+                MacUILib_printf("%c",tempPos.symbol);
+              }
+              else 
+              {
+                MacUILib_printf(" ");
+              }
+              
+          }
+          MacUILib_printf("\n");
+      }   
+
+    MacUILib_printf("Score: %d\n", myGameMech->getScore());
+    MacUILib_printf("Player Pos: [%d %d]\n", tempPos.x, tempPos.y);
+
+    MacUILib_printf("To exit the game, press the ESC button!");
 
 }
 
@@ -68,7 +134,19 @@ void LoopDelay(void)
 
 void CleanUp(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();   
+
+    if (myGameMech->getExitFlagStatus())
+    {
+         MacUILib_printf("\nYou pressed ESC. Closing the program..."); 
+    } 
+
+    else if (myGameMech->getLoseFlagStatus())
+    {
+         MacUILib_printf("\nYOU LOST. Game is over..."); 
+    } 
   
     MacUILib_uninit();
+    delete myGameMech;
+    delete myPlayer; 
 }
